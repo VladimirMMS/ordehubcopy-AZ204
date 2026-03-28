914048c952058983dcfb40fa145a8a663d047a4b
Vladimir Mercado Santana <vladimir@192.168.1.3>
2026-03-28 10:46:54 -0400

    Semana 1 - bootstrap React + NestJS

---
+++ src/api/src/main.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  const config = new DocumentBuilder()
+    .setTitle('OrderHub API')
+    .setDescription('API base para la Semana 1 de AZ-204')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+bootstrap();
